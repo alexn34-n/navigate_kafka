c2dddbaa072286f618343f9d093417693663dc13
alexn <alexn3469@mail.ru>
2022-10-16 11:27:37 +0300

    Init commit

---
+++ common/src/test/java/com/nagigate/ship/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagigate.ship.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
